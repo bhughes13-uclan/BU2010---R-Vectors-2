5169457d33c0b08806318cc4716e5c149abb6d7b
bhughes13-uclan <115146381+bhughes13-uclan@users.noreply.github.com>
2022-12-08 10:47:55 +0000

    Updated Modules Vectores
    I have added module vector, book cost. I have then named Book costs and printed

--- vectors.R
+++ vectors.R
@@ -1,21 +1,21 @@
 # Set up a vector named modules consisting of the module codes of all modules you study this year. 
 
-modules <- c("BU2010", "xxx")
+modules <- c("BU2010", "EC2401", "BU2306", "BU2047", "BU2103", "BU2011")
 
 
 # Find out the cost of the main textbook from each of these modules. 
 # Set up a vector named book_costs consisting of the book costs. Use the same order as you did in the modules vector. 
 
-book_costs <-
+book_costs <- c(59.59, 47.99, 71.99, 32.80, 64.98, 45.70)
 
 
 # Set modules as the names of book_costs.
 
-names(
+names(book_costs) <- modules
 
   
 # Display book_costs
-  
+  book_costs
 
 
 
